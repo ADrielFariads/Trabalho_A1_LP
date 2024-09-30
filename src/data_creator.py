4ab34ece70c2faa6e0116e991f2c1827e0ad5465
adriel <adriel25dias@gmail.com>
2024-09-30 18:43:00 -0300

    add game_matrix function

--- src/data_creator.py
+++ src/data_creator.py
@@ -1,34 +1,139 @@
 import pandas as pd
 import numpy as np
 import re
+import doctest
 
 
-def count_moves(game, piece):
-  
-  pieces = {
-    "pawn": [" a", " b", " c", " d", " e", " f", " g", " h"],
-    "king": "K",
-    "queen": "Q",
-    "knight": "N",
-    "bishop": "B",
-    "rook": "R"
-}
-  
-  try:
-      if piece.lower() == "pawn":
-          result = 0
-          if game[0] in "a b c d e f g h".split():
-            result +=1
-          for each in pieces["pawn"]:
-              moves = game.count(each)
-              result += moves
-          return result
-      else:
-          piece = piece.lower()
-          piece_code = pieces[piece]
+def count_moves(game:str, piece:str) -> int:
+    """
+    Counts the number of moves made by a specific chess piece in a game represented in algebraic notation.
 
-          moves = game.count(piece_code)
-          return moves
-  except KeyError:
-    print("Houve um erro, selecione um nome válido de peça")
-    return None
+    Parameters:
+    ----------
+    game : str
+        A string representing a chess game in algebraic notation.
+        Example:
+        "e4 e5 Nf3 Nc6 Bb5 a6 Ba4 Nf6 O-O Be7 d4 d6 c3 O-O Re1 Nb8 Bc2 Nbd7"
+    
+    piece : str
+        The name of the chess piece to count moves for.
+        Example: "pawn"
+    Returns:
+    -------
+    int
+        The number of moves made by the specified chess piece in the game.
+
+    None
+        If an invalid piece name is provided, the function returns None and prints an error message.
+
+    Examples
+    --------
+    >>> game = "e4 e5 Nf3 Nc6 Bb5 a6 Ba4 Nf6 O-O Be7 d4 d6 c3 O-O Re1 Nb8 Bc2 Nbd7 Nbd2 c6"
+    >>> count_moves(game, "pawn")
+    8
+    >>> count_moves(game, "knight")
+    4
+    >>> count_moves(game, "king")
+    1
+    >>> count_moves(game, "dragon")
+    Houve um erro, selecione um nome válido de peça
+    None
+        """
+    
+    pieces = {
+        "pawn": [" a", " b", " c", " d", " e", " f", " g", " h"],
+        "king": "K",
+        "queen": "Q",
+        "knight": "N",
+        "bishop": "B",
+        "rook": "R"
+    }
+    
+    try:
+        if piece.lower() == "pawn":
+            result = 0
+            if game[0] in "a b c d e f g h".split():
+                result +=1
+            for each in pieces["pawn"]:
+                moves = game.count(each)
+                result += moves
+            return result
+        else:
+            piece = piece.lower()
+            piece_code = pieces[piece]
+
+            moves = game.count(piece_code)
+            return moves
+    except KeyError:
+        print("Houve um erro, selecione um nome válido de peça")
+        return None
+
+def game_matrix(game:str) -> np.array:
+    """
+    creates an 8x8 matrix which represents the most visited squares in the chess game
+
+    Parameters:
+
+    ----------
+    game: str
+        a string which represents the following game with algebrial notation
+        Example:
+            "e4 e5 Nf3 Nc6 Bb5 a6 Ba4 Nf6 O-O Be7 d4 d6 c3 O-O Re1 Nb8 Bc2 Nbd7"
+
+    Returns: 
+    ----------
+    np.array
+        An array which represent the number of times each square was visited 
+
+
+    """
+    squares = {"a":0,
+               "b":1, 
+               "c":2, 
+               "d":3, 
+               "e":4, 
+               "f":5, 
+               "g":6, 
+               "h":7}
+
+    matrix = np.zeros((8,8), dtype=int)
+
+    match_squares = re.sub(r'[KQNRBx+#=]', "", game)
+    moves = match_squares.split()
+
+    for i, each in enumerate(moves):
+
+        if each == "1/2-1/2":
+            continue
+
+        elif each == "O-O":
+            if i % 2 == 1:
+                matrix[7][6] += 1
+                matrix[7][5] += 1
+            else:
+                matrix[0][6] += 1
+                matrix[0][5] += 1
+
+        elif each == "O-O-O":
+            if i % 2 == 1:
+                matrix[7][2] += 1
+                matrix[7][3] += 1
+            else:
+                matrix[0][2] += 1
+                matrix[0][3] += 1
+
+
+            
+        elif each[0] in squares.keys():
+            column = squares[each[0]]
+            row = 8 - int(each[-1])
+            matrix[row][column] += 1
+        else:
+            continue
+
+
+
+    return matrix
+
+
+
--- tests/test_data_creator.py
+++ tests/test_data_creator.py
@@ -8,6 +8,7 @@ import data_creator
 
 class Test_count_moves(unittest.TestCase):
 
+    #testa a função em casos normais
     def test_common_game_pawn(self):
         example = "e4 e5 Nf3 Nc6 Bb5 a6 Ba4 Nf6 O-O Be7 d4 d6 c3 O-O Re1 Nb8 Bc2 Nbd7 Nbd2 c6 Nf1 Qc7 Ng3 g6 Bh6 Re8 Qd2 Bf8 Bxf8 Nxf8 Qg5 Qe7 h4 h6 Qxh6 Ng4 Qd2 Kg7 h5 Nh7 dxe5 dxe5 Bb3 Be6 Bxe6 Qxe6 Rad1 Rad8 Qxd8 Rxd8 Rxd8 Qxa2 Rd7 Qxb2 hxg6 Qxf2+ Kh1 Qxg3 Rf7+ Kxg6 Rxb7 Ng5 Rf1 Nxf3 Rxf3 Qh4+ Kg1 Qe1+ Rf1 Qe3+ Kh1 Qh6+ Kg1 Qh2#"
         result = data_creator.count_moves(example, "pawn")
@@ -17,8 +18,41 @@ class Test_count_moves(unittest.TestCase):
         example = "e4 e5 Nf3 Nc6 Bb5 a6 Ba4 Nf6 O-O Be7 d3 d6 c3 O-O Nbd2 Nb8 Re1 Nbd7 Nf1 c6 Ng3 g6 d4 Qc7 h3 Re8 Bb3 Bf8 Ng5 Re7 f4 Bg7 f5 Nf8 Rf1 d5 Qf3 exd4 cxd4 dxe4 N3xe4 Bxf5 Bg5 Rxe4 Qxe4 Bxe4 Bf4 Qb6 Bxf7+ Kh8 Be5 Bxe5 Rae1 Qxd4+ Kh1 Qd2 Kh1 Qg3 Rxb7 Qh2#"
         result = data_creator.count_moves(example, "knight")
         self.assertEqual(result, 11)
-        
-    
+
+    def test_common_game_bishop(self):
+        example = "e4 e5 Nf3 Nc6 Bb5 a6 Ba4 Nf6 O-O Be7 d3 d6 c3 O-O Nbd2 Nb8 Re1 Nbd7 Nf1 c6 Ng3 g6 d4 Qc7 h3 Re8 Bb3 Bf8 Ng5 Re7 f4 Bg7 f5 Nf8 Rf1 d5 Qf3 exd4 cxd4 dxe4 N3xe4 Bxf5 Bg5 Rxe4 Qxe4 Bxe4 Bf4 Qb6 Bxf7+ Kh8 Be5 Bxe5 Rae1 Qxd4+ Kh1 Qd2 Kh1 Qg3 Rxb7 Qh2#"
+        result = data_creator.count_moves(example, "bishop")
+        self.assertEqual(result, 13)
+
+    def test_common_game_rook(self):
+        example = "d4 d5 c4 c6 Nf3 Nf6 Nc3 dxc4 a4 Bf5 e3 e6 Bxc4 Bb4 O-O O-O Qe2 Nbd7 Rd1 Qc7 h3 e5 dxe5 Nxe5 Nxe5 Qxe5 Bd2 Rad8 Be1 Bd6 f4 Qe7 Bh4 Bc5 Rxd8 Rxd8 Re1 Qd6 Kh1 Bg6 e4 Qxf4 Bxf6 Qxf6 e5 Qe7 Ne4 Bd4 Nd6 Bxe5 Qxe5 Qxd6"
+        result = data_creator.count_moves(example, "rook")
+        self.assertEqual(result, 5)
+
+    def test_common_game_queen(self):
+        example = "d4 d5 c4 c6 Nf3 Nf6 Nc3 dxc4 a4 Bf5 e3 e6 Bxc4 Bb4 O-O O-O Qe2 Nbd7 Rd1 Qc7 h3 e5 dxe5 Nxe5 Nxe5 Qxe5 Bd2 Rad8 Be1 Bd6 f4 Qe7 Bh4 Bc5 Rxd8 Rxd8 Re1 Qd6 Kh1 Bg6 e4 Qxf4 Bxf6 Qxf6 e5 Qe7 Ne4 Bd4 Nd6 Bxe5 Qxe5 Qxd6"
+        result = data_creator.count_moves(example, "queen")
+        self.assertEqual(result, 10)
+
+    def test_common_game_king(self):
+
+        example = "e4 c5 Nf3 d6 d4 cxd4 Nxd4 Nf6 Nc3 a6 Be2 e5 Nb3 Be7 O-O O-O Be3 Be6 f4 exf4 Bxf4 Nc6 Kh1 Qc7 Nd4 Nxd4 Qxd4 Rad8 Rad1 b5 a3 Rfe8 Bf3 Bc4 Rfe1 Nd7 Qf2 Ne5 Be3 Nxf3 Qxf3 Bf6 Bd4 Bxd4 Rxd4 d5 Red1 dxe4 Rxd8 Rxd8 Rxd8+ Qxd8 Nxe4 Bd5 Qf4 h6 Nc3 Bc6 h3 Qe7 Kh2 Qe6 Qd4 a5 Qf4 b4 axb4 axb4 Nd1 b3 c3 Qe2 Nf2 Qxb2 Qb8+ Kh7 Qf4 Qc2 Qd4 b2 Qd3+ Qxd3 Nxd3 b1=Q"
+        result = data_creator.count_moves(example, "king")
+        self.assertEqual(result, 3)
+
+    #testa a função para um nome de peça que não existe
+    def test_not_a_piece_name(self):
+
+        example = "e4 c5 Nf3 d6 d4 cxd4 Nxd4 Nf6 Nc3 a6 Be2 e5 Nb3 Be7 O-O O-O Be3 Be6 f4 exf4 Bxf4 Nc6 Kh1 Qc7 Nd4 Nxd4 Qxd4 Rad8 Rad1 b5 a3 Rfe8 Bf3 Bc4 Rfe1 Nd7 Qf2 Ne5 Be3 Nxf3 Qxf3 Bf6 Bd4 Bxd4 Rxd4 d5 Red1 dxe4 Rxd8 Rxd8 Rxd8+ Qxd8 Nxe4 Bd5 Qf4 h6 Nc3 Bc6 h3 Qe7 Kh2 Qe6 Qd4 a5 Qf4 b4 axb4 axb4 Nd1 b3 c3 Qe2 Nf2 Qxb2 Qb8+ Kh7 Qf4 Qc2 Qd4 b2 Qd3+ Qxd3 Nxd3 b1=Q"
+        result = data_creator.count_moves(example, "viking")
+        self.assertEqual(result, None)
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
