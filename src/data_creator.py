2ba577914a7b2109c17e79afc0479243dfae7f41
adriel <adriel25dias@gmail.com>
2024-10-02 18:57:26 -0300

    rename some modules

--- src/data_creator.py
+++ src/data_creator.py
@@ -1,3 +1,7 @@
+""""
+Functions for making new data
+"""
+
 import pandas as pd
 import numpy as np
 import re
---
+++ src/graph_creator.py
@@ -0,0 +1,13 @@
+"""
+Functions for graphs and visual features
+"""
+
+import pandas as pd
+import numpy as np
+import doctest
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+import data_creator
