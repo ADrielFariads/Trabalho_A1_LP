5a190e4b2306085728a9a3a5d478261b9469c85e
joaozin <joao.resende@for-tech.co>
2024-10-07 20:54:27 -0300

    delete gpt

--- src/main.py
+++ src/main.py
@@ -1,11 +1,5 @@
-import pandas as pd
 import data
 import game
-import Constants
-
-
-key = input("Insira uma chave openIA: ")
-Constants.Constants.key = key
 
 df = data.read_data_set()
 data.shape_data_set(df)
