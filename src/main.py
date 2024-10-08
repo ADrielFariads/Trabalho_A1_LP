892bd59918528a34eb18e31a5c35149b0aa32c21
gschuenker <gabrielschuenker@outlook.com>
2024-10-08 10:35:59 -0300

    Rename data module to data_cleaner

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
-import data
+import Trabalho_A1.Trabalho_A1_LP.src.data_cleaner as data_cleaner
 import game
 
-df = data.read_data_set()
-data.shape_data_set(df)
-df = data.cut_short_games(df)
+df = data_cleaner.read_data_set()
+data_cleaner.shape_data_set(df)
+df = data_cleaner.cut_short_games(df)
 games = game.game_info(df)
 game.stock_fish(games)
 
