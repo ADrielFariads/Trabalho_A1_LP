f5df7aade04215b897a6d07d2658d22ab048c185
Gustavo Luciano dos Santos <gustavuluciano@gmail.com>
2024-10-09 19:44:42 -0300

    add cut duplicate func

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 import Trabalho_A1.Trabalho_A1_LP.src.data_cleaner as data_cleaner
 import game
 
+
 df = data_cleaner.read_data_set()
 data_cleaner.add_black_white_level(df)
+df = data_cleaner.cut_duplicates(df)
 df = data_cleaner.cut_short_games(df)
 games = game.game_info(df)
 game.stock_fish(games)
 
-
+print(df)
 
 
 
