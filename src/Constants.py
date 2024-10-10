8864e9338ad5bae9c7370506fa02b14266d0af4c
adriel <adriel25dias@gmail.com>
2024-10-10 09:10:21 -0300

    merged branch

--- src/Constants.py
+++ src/Constants.py
@@ -2,10 +2,6 @@ import os
 
 class Constants:
     path = os.path.join(os.getcwd() , "data", "games.csv")
-<<<<<<< HEAD
 
     stockfish_path = r"" #put here the stockfish's path
 
-=======
-    stockfish_path = r"c:\\Users\\joaov\\Downloads\\stockfish\\stockfish-windows-x86-64-avx2.exe"
->>>>>>> ff9263c2a0bc9264bd49b6eab5c152f74c678aa4
