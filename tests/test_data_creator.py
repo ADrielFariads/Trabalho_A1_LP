7078e48c001b3bfa7bcc43426c71f02cd86b8395
adriel <adriel25dias@gmail.com>
2024-10-11 08:46:06 -0300

    fixed some tests

--- tests/test_data_creator.py
+++ tests/test_data_creator.py
@@ -9,7 +9,7 @@ import data_creator
 
 class Test_count_moves(unittest.TestCase):
 
-    #testa a função em casos normais
+    #tests the function in common cases
     def test_common_game_pawn(self):
         example = "e4 e5 Nf3 Nc6 Bb5 a6 Ba4 Nf6 O-O Be7 d4 d6 c3 O-O Re1 Nb8 Bc2 Nbd7 Nbd2 c6 Nf1 Qc7 Ng3 g6 Bh6 Re8 Qd2 Bf8 Bxf8 Nxf8 Qg5 Qe7 h4 h6 Qxh6 Ng4 Qd2 Kg7 h5 Nh7 dxe5 dxe5 Bb3 Be6 Bxe6 Qxe6 Rad1 Rad8 Qxd8 Rxd8 Rxd8 Qxa2 Rd7 Qxb2 hxg6 Qxf2+ Kh1 Qxg3 Rf7+ Kxg6 Rxb7 Ng5 Rf1 Nxf3 Rxf3 Qh4+ Kg1 Qe1+ Rf1 Qe3+ Kh1 Qh6+ Kg1 Qh2#"
         result = data_creator.count_moves(example, "pawn")
@@ -41,7 +41,7 @@ class Test_count_moves(unittest.TestCase):
         result = data_creator.count_moves(example, "king")
         self.assertEqual(result, 3)
 
-    #testa a função para um nome de peça que não existe
+    #tests the function for a wrong key-name
     def test_not_a_piece_name(self):
 
         example = "e4 c5 Nf3 d6 d4 cxd4 Nxd4 Nf6 Nc3 a6 Be2 e5 Nb3 Be7 O-O O-O Be3 Be6 f4 exf4 Bxf4 Nc6 Kh1 Qc7 Nd4 Nxd4 Qxd4 Rad8 Rad1 b5 a3 Rfe8 Bf3 Bc4 Rfe1 Nd7 Qf2 Ne5 Be3 Nxf3 Qxf3 Bf6 Bd4 Bxd4 Rxd4 d5 Red1 dxe4 Rxd8 Rxd8 Rxd8+ Qxd8 Nxe4 Bd5 Qf4 h6 Nc3 Bc6 h3 Qe7 Kh2 Qe6 Qd4 a5 Qf4 b4 axb4 axb4 Nd1 b3 c3 Qe2 Nf2 Qxb2 Qb8+ Kh7 Qf4 Qc2 Qd4 b2 Qd3+ Qxd3 Nxd3 b1=Q"
@@ -50,9 +50,9 @@ class Test_count_moves(unittest.TestCase):
 
 
 
-class Test_game_matriz(unittest.TestCase):
+class Test_game_matrix(unittest.TestCase):
 
-    #testa a função para um jogo genérico
+    #tests the function for a common game
     def test_common_game(self):
         example = "e4 e5 Nf3 Nc6 Bb5 a6 Ba4 Nf6 O-O Be7 d4 d6 c3 O-O Re1 Nb8 Bc2 Nbd7 Nbd2 c6 Nf1 Qc7 Ng3 g6 Bh6 Re8 Qd2 Bf8 Bxf8 Nxf8 Qg5 Qe7 h4 h6 Qxh6 Ng4 Qd2 Kg7 h5 Nh7 dxe5 dxe5 Bb3 Be6 Bxe6 Qxe6 Rad1 Rad8 Qxd8 Rxd8 Rxd8 Qxa2 Rd7 Qxb2 hxg6 Qxf2+ Kh1 Qxg3 Rf7+ Kxg6 Rxb7 Ng5 Rf1 Nxf3 Rxf3 Qh4+ Kg1 Qe1+ Rf1 Qe3+ Kh1 Qh6+ Kg1 Qh2#"
         result = data_creator.game_matrix(example)
@@ -65,7 +65,7 @@ class Test_game_matriz(unittest.TestCase):
        [1, 2, 1, 2, 0, 1, 0, 1],
        [1, 0, 0, 0, 2, 4, 3, 2]])
         
-        np.testing.assert_equal(result, expected_result) #função para comparar arrays
+        np.testing.assert_equal(result, expected_result) #function for compare arrays
 
 
     def test_not_a_string(self):
